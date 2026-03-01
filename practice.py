5b05bbaa0d418dde94eb9d6d4dda7df95f7c7ef7
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-02 00:09:42 +0800

    Added two more steps, and cheerful messages to encourage self.

--- practice.py
+++ practice.py
@@ -3,3 +3,6 @@
 #How to add a file on a repository?
 #First, you need to make a repo, make sure that you use a snake coding casing style.
 #Second, you should have your Termux and Quick Edit ready.
+#Third, make sure that you know the git commands.
+#HEHEHEHEHE AND MAG-IMBAK NG PASENSYA AT CREATIVE JUICES.
+#HAPPY LEARNING, MENDEZ!
